5b4c5c3778c7f41cefc8b135448707d697e84b82
Jon Bracy <jonbracy@gmail.com>
2015-12-21 18:33:31 -0800

    Start moving to Rails 5!! Choo Choo

--- activerecord-filter.gemspec
+++ activerecord-filter.gemspec
@@ -18,7 +18,7 @@ Gem::Specification.new do |spec|
   spec.require_paths = ["lib"]
 
   spec.add_runtime_dependency 'pg'
-  spec.add_runtime_dependency 'activerecord', '~> 4.0'
+  spec.add_runtime_dependency 'activerecord', '~> 5.0.0.beta1'
     
   spec.add_development_dependency "bundler"
   spec.add_development_dependency "rake"
--- ext/active_record/query_methods.rb
+++ ext/active_record/query_methods.rb
@@ -1,24 +1,24 @@
-module ActiveRecord::QueryMethods
-  
-  # # TODO: testme and rename to joins?
-  # def joined?(assoc)
-  #   joined_assocs = joins_values.map{ |i| i.is_a?(Hash) ? i.keys : i.to_sym }.flatten
-  #   joined_assocs.include?(assoc)
-  # end
-  #
-  # # TODO: testme and rename to includes?
-  # def included?(assoc)
-  #   included_assocs = includes_values.map{ |i| i.is_a?(Hash) ? i.keys : i.to_sym }.flatten
-  #   included_assocs.include?(assoc)
-  # end
-  
-  # TODO: testme and rename to 
-  def references?(assoc)
-    references_assocs = references_values.map{ |i| i.is_a?(Hash) ? i.keys : i.to_sym }.flatten
-    references_assocs.include?(assoc)
-  end
-  
-end
+# module ActiveRecord::QueryMethods
+#
+#   # # TODO: testme and rename to joins?
+#   # def joined?(assoc)
+#   #   joined_assocs = joins_values.map{ |i| i.is_a?(Hash) ? i.keys : i.to_sym }.flatten
+#   #   joined_assocs.include?(assoc)
+#   # end
+#   #
+#   # # TODO: testme and rename to includes?
+#   # def included?(assoc)
+#   #   included_assocs = includes_values.map{ |i| i.is_a?(Hash) ? i.keys : i.to_sym }.flatten
+#   #   included_assocs.include?(assoc)
+#   # end
+#
+#   # TODO: testme and rename to
+#   def references?(assoc)
+#     references_assocs = references_values.map{ |i| i.is_a?(Hash) ? i.keys : i.to_sym }.flatten
+#     references_assocs.include?(assoc)
+#   end
+#
+# end
 
 module ActiveRecord::Querying
   # delegate :joined?, :to => :all
