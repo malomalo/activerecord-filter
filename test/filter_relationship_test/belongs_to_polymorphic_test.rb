48f820fa0a77876cacae68d50c1eeb922de7c81f
Jon Bracy <jonbracy@gmail.com>
2020-03-12 10:20:17 -0500

    Update test

--- test/filter_relationship_test/belongs_to_polymorphic_test.rb
+++ test/filter_relationship_test/belongs_to_polymorphic_test.rb
@@ -42,11 +42,12 @@ class BelongsToPolymorphicFilterTest < ActiveSupport::TestCase
   test '::filter with seperate joins' do
     query = View.filter(subject: {as: "BelongsToPolymorphicFilterTest::Property", name: 'Name'}, account: {name: 'Account'})
     assert_sql(<<-SQL, query)
-      SELECT views.* FROM views
-      LEFT OUTER JOIN accounts
-        ON accounts.id = views.account_id
+      SELECT views.*
+      FROM views
       LEFT OUTER JOIN properties properties_as_subject
         ON properties_as_subject.id = views.subject_id AND views.subject_type = 'BelongsToPolymorphicFilterTest::Property'
+      LEFT OUTER JOIN accounts
+        ON accounts.id = views.account_id
       WHERE properties_as_subject.name = 'Name' AND accounts.name = 'Account'
     SQL
   end
