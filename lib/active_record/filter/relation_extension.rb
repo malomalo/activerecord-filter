47a45f8b2f10a406419562a80c670958eb62e81c
Jon Bracy <jonbracy@gmail.com>
2025-12-26 08:58:21 -0600

    Update CI

--- lib/active_record/filter/relation_extension.rb
+++ lib/active_record/filter/relation_extension.rb
@@ -51,7 +51,7 @@ module ActiveRecord::Filter::RelationExtension
     @filter_clause_factory ||= ActiveRecord::Filter::FilterClauseFactory.new(klass, predicate_builder)
   end
 
-  if ActiveRecord.version >= "7.2"
+  if ActiveRecord.version < "8.1" && ActiveRecord.version >= "7.2"
     def build_arel(connection, aliases = nil)
       arel = super
       my_alias_tracker = ActiveRecord::Associations::AliasTracker.create(model.connection_pool, table.name, [])
